368c712b9c53e5eb9afd5aaa0b03016a27b41315
VinniFachini <vinniebrasil@gmail.com>
2024-10-31 17:45:38 -0300

    fix: container

--- pages/pacientes/index.vue
+++ pages/pacientes/index.vue
@@ -2,30 +2,30 @@
   <div class="flex flex-col items-start justify-between min-h-screen">
     <Header />
 
-    <div
-      class="main-content flex-grow flex flex-col items-center justify-start p-6 max-w-4xl"
-    >
-      <h1 class="text-4xl font-bold text-indigo-600 mb-4">Pacientes</h1>
-      <button
-        @click="newPaciente"
-        class="my-4 bg-indigo-600 text-white font-semibold py-2 px-4 rounded hover:bg-indigo-700 transition-colors"
-      >
-        Novo Paciente
-      </button>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div
-          v-for="paciente in pacientes"
-          :key="paciente.id"
-          class="bg-white border border-gray-200 rounded-lg shadow-md p-4"
+    <div class="main-content flex-grow w-full flex flex-col justify-start p-6">
+      <div class="w-full">
+        <h1 class="text-4xl font-bold text-indigo-600 mb-4">Pacientes</h1>
+        <button
+          @click="newPaciente"
+          class="my-4 bg-indigo-600 text-white font-semibold py-2 px-4 rounded hover:bg-indigo-700 transition-colors"
         >
-          <h2 class="text-xl font-semibold text-gray-800">{{ paciente.nome }}</h2>
-          <p class="text-gray-600"><strong>CPF:</strong> {{ paciente.cpf }}</p>
-          <button
-            @click="viewDetails(paciente.id)"
-            class="mt-2 w-full bg-indigo-600 text-white font-semibold py-2 rounded hover:bg-indigo-700 transition-colors"
+          Novo Paciente
+        </button>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div
+            v-for="paciente in pacientes"
+            :key="paciente.id"
+            class="bg-white border border-gray-200 rounded-lg shadow-md p-4"
           >
-            Ver Detalhes
-          </button>
+            <h2 class="text-xl font-semibold text-gray-800">{{ paciente.nome }}</h2>
+            <p class="text-gray-600"><strong>CPF:</strong> {{ paciente.cpf }}</p>
+            <button
+              @click="viewDetails(paciente.id)"
+              class="mt-2 w-full bg-indigo-600 text-white font-semibold py-2 rounded hover:bg-indigo-700 transition-colors"
+            >
+              Ver Detalhes
+            </button>
+          </div>
         </div>
       </div>
     </div>
